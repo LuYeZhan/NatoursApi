f83b8ce3331dc66e961b71ae61a2733dd2d697cd
Ye Zhan <lu@hooli.store>
2020-06-05 16:44:18 +0200

    implementing CORS

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +26,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) Global middlewares:
+
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
 // access to static files on our public folder, serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
